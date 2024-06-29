bd200eae93f4880ac409ff594b6c82a43a52e2e9
lifghi <duitrabit10rb@gmail.com>
2024-06-29 21:08:18 +0800

    modul5

---
+++ 2210817310014_FATIH_ALIF_GHIFARI_PRAKMODUL5/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/NavigationHost.kt
@@ -0,0 +1,8 @@
+package com.google.codelabs.mdc.kotlin.shrine
+
+import androidx.fragment.app.Fragment
+
+interface NavigationHost {
+
+    fun navigateTo(fragment: Fragment, addToBackstack: Boolean)
+}
